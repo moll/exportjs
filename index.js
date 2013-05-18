cb070c51e880057512ce91368f419ad3c193f5f0
Andri Möll <andri@dot.ee>
2013-05-18 21:14:10 +0300

    Make iffy primary. IIFE does not roll off the tongue at all.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var _ = require("underscore")
 var File = require("fs")
 
-exports.Iife = function(opts) {
+exports.Iffy = function(opts) {
   this.opts = opts
-  this.template = getTemplate("iife")
+  this.template = getTemplate("iffy")
 }
 
-exports.Iife.prototype.tab = "  "
+exports.Iffy.prototype.tab = "  "
 
-exports.Iife.prototype.wrap = function(file) {
+exports.Iffy.prototype.wrap = function(file) {
   var source = File.readFileSync(file, "utf8")
   var vars = {}
   if (this.opts.global) vars.global = "this", vars.root = "this"
   return this.template({source: this.indent(source), vars: vars})
 }
 
-exports.Iife.prototype.indent = function(source) {
+exports.Iffy.prototype.indent = function(source) {
   return source.replace(/^(?!$)/gm, this.tab)
 }
 
